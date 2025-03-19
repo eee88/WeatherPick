8bcfbbf6a456d2bb8330a3f2e6a2548a2e570398
tjddn <sungwk100@gmail.com>
2025-03-19 21:59:36 +0900

    test

---
+++ src/test/java/Weather/WeatherPcik/WeatherPcikApplicationTests.java
@@ -0,0 +1,13 @@
+package Weather.WeatherPcik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherPcikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
